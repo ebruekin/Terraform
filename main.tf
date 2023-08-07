940afd102756e4a0177746f377044d8774da78bc
Ebru Ekin <54847953+ebruekin@users.noreply.github.com>
2023-08-07 15:08:06 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,13 +27,13 @@ resource "azurerm_service_plan" "service-plan" {
 }
 
 resource "azurerm_windows_web_app" "web-app" {
-  name                = "development-webapp1"
+  name                = "development-webapp2"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_service_plan.service-plan.location
   service_plan_id     = azurerm_service_plan.service-plan.id
 
   app_settings = {
-    "APPLICATIONINSIGHTS_CONNECTION_STRING"      = "InstrumentationKey=5dfdb1d1-5d6f-4ffd-8636-212e36b41b73;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/"
+    
     "ApplicationInsightsAgent_EXTENSION_VERSION" = "~3"
     "XDT_MicrosoftApplicationInsights_Mode"      = "Recommended"
   }
@@ -59,19 +59,17 @@ resource "azurerm_application_insights" "dev-insights" {
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
   sampling_percentage = 0
-  workspace_id        = "/subscriptions/5eebd5ca-e3d3-4530-abea-45a3dfd1af8c/resourceGroups/DefaultResourceGroup-EUS/providers/Microsoft.OperationalInsights/workspaces/DefaultWorkspace-5eebd5ca-e3d3-4530-abea-45a3dfd1af8c-EUS"
+ 
 }
 
 resource "azurerm_postgresql_flexible_server" "flexible-postgre" {
-  name                   = "dev-db1997"
+  name                   = "devdb"
   resource_group_name    = azurerm_resource_group.rg.name
   location               = azurerm_resource_group.rg.location
   version                = "15"
-  administrator_login    = "ebruekin"
-  administrator_password = "Zm2e8qrzq."
+
   backup_retention_days  = 7
-  // fqdn                          = "dev-db1997.postgres.database.azure.com"
-  //id                            = "/subscriptions/5eebd5ca-e3d3-4530-abea-45a3dfd1af8c/resourceGroups/development/providers/Microsoft.DBforPostgreSQL/flexibleServers/dev-db1997"
+  
   //public_network_access_enabled = true
   sku_name   = "B_Standard_B1ms"
   tags       = {}
